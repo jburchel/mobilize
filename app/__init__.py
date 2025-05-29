d17e757776d97cc855a847bfdb82ebb647d7e088
jburchel <jburchel@gmail.com>
2025-05-29 13:36:41 -0400

    Refactor: Simplify Google OAuth credential loading in production

--- app/__init__.py
+++ app/__init__.py
@@ -109,8 +109,8 @@ def create_app(test_config=None):
         app.config.update(
             SECRET_KEY=secrets.get('SECRET_KEY', app.config.get('SECRET_KEY')),
             SQLALCHEMY_DATABASE_URI=db_uri,
-            GOOGLE_CLIENT_ID=secrets.get('GOOGLE_CLIENT_ID', os.environ.get('mobilize-google-client-id')),
-            GOOGLE_CLIENT_SECRET=secrets.get('GOOGLE_CLIENT_SECRET', os.environ.get('mobilize-google-client-secret')),
+            GOOGLE_CLIENT_ID=os.environ.get('mobilize-google-client-id'),
+            GOOGLE_CLIENT_SECRET=os.environ.get('mobilize-google-client-secret'),
         )
         app.logger.info(f'Database URI set to: {db_uri and db_uri[:10]}...')
     elif env == 'testing':
