acb0d87dc98ca12b5ecaf0cef70eb5fed1fc00ea
jburchel <jburchel@gmail.com>
2025-06-03 09:10:41 -0400

    Simplify main.py to remove imports of missing models

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,14 +1,5 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, current_app
+from flask import Blueprint, redirect, url_for
 from flask_login import login_required, current_user
-from app.models.user import User
-from app.models.task import Task
-from app.models.church import Church
-from app.models.person import Person
-from app.models.pipeline import Pipeline, PipelineStage
-from app.models.assignment import Assignment
-from app import db
-from sqlalchemy import func, desc
-import datetime
 
 # Create Blueprint
 main_bp = Blueprint('main', __name__)
